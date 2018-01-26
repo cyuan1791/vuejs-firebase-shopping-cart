9d229149557ca126afe9f40eff1fefe9327e3b4b
cmsnow <cyuan123@live.com>
2018-01-26 11:39:36 -0800

    update

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,9 +2,15 @@ import firebase from 'firebase'
 
 
 const config = {
-	apiKey: "AIzaSyAvE8yZMbjt6Y799fAHFOOJ-WhjdwGfK6A",
-	authDomain: "vuejs-shopping-cart.firebaseapp.com",
-	databaseURL: "https://vuejs-shopping-cart.firebaseio.com/",
+	// apiKey: "AIzaSyAvE8yZMbjt6Y799fAHFOOJ-WhjdwGfK6A",
+	// authDomain: "vuejs-shopping-cart.firebaseapp.com",
+	// databaseURL: "https://vuejs-shopping-cart.firebaseio.com/",
+  apiKey: "AIzaSyCmN4hwCfPjjQ2spsO1UxH1aOfvmkq2yR8",
+  authDomain: "auth-test-c7cdb.firebaseapp.com",
+  databaseURL: "https://auth-test-c7cdb.firebaseio.com",
+  projectId: "auth-test-c7cdb",
+  storageBucket: "auth-test-c7cdb.appspot.com",
+  messagingSenderId: "958475560180"
 }
 
 firebase.initializeApp(config);
